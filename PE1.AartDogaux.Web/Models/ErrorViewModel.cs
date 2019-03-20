315b54c1265ed0b9c5f6569c5553b54862ad91c3
aartdognaux <aartdognaux@hotmail.com>
2019-03-20 09:48:17 +0100

    Add project files.

---
+++ PE1.AartDogaux.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PE1.AartDogaux.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
